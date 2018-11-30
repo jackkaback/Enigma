8d33193be1d1459793b82d1af67784d559e8fbcf
jack <jackkaback@gmail.com>
2018-11-29 19:02:44 -0500

    Added rotor object

---
+++ rotor.java
@@ -0,0 +1,9 @@
+public class rotor{
+
+	//Holds the wirring set up for the rotors
+	private int[][] wiring;
+
+	//The position in which the rotor advances the next rotor
+	private int rotorAdvancePos;
+
+}
